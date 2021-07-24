403fddc2f12df860d0b903ed0cd69311b475576f
Jebakani <vjishwarya@gmail.com>
2021-07-24 11:45:36 +0530

    [JEBAKANI] Creating Person Data Management using Collection

---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating Person Data Management System");
+            Console.Write();
+        }
+    }
+}
